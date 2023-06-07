2d7df2344318bfe2ece3858f4232771dc1beecc4
mike <lixiaofeng1115@outlook.com>
2023-06-07 23:13:59 +0800

    3: gigasecond

---
+++ gigasecond/src/lib.rs
@@ -0,0 +1,10 @@
+use time::{Duration, PrimitiveDateTime as DateTime};
+
+// Returns a DateTime one billion seconds after start.
+pub fn after(start: DateTime) -> DateTime {
+    let interval = 1000_000_000;
+
+    let duration = Duration::new(interval, 0);
+
+    start + duration
+}
