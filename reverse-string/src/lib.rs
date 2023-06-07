4a6637c0353c772f8c49d32b9c7c5f306d8a4b11
mike <lixiaofeng1115@outlook.com>
2023-06-07 23:21:40 +0800

    2: reverse-string, optimize

--- reverse-string/src/lib.rs
+++ reverse-string/src/lib.rs
@@ -4,7 +4,7 @@ pub fn reverse(input: &str) -> String {
     let mut res = String::new();
 
     for char in input.graphemes(true).rev() {
-        res.push_str(char)
+        res += char;
     }
 
     res
