f5d6498b939f748ac4efba0c993026517024105f
mike <lixiaofeng1115@outlook.com>
2023-06-11 23:41:21 +0800

    3: clock, optimize

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,47 +1,34 @@
 #![allow(unused)]
 use std::fmt::{Display, Formatter};
 
-const MINUTES_PER_HOUR: i32 = 60;
-const HOURS_PER_DAY: i32 = 24;
+const MINUTES_PER_HOUR: i64 = 60;
+const MINUTES_PER_DAY: i64 = 24 * 60;
 
 #[derive(Debug, PartialEq)]
 pub struct Clock {
-    hours: i32,
-    minutes: i32,
+    minutes: i64,
 }
 
 impl Clock {
-    pub fn new(hours: i32, minutes: i32) -> Self {
-        let mut res_hours = 0;
-        let mut res_minutes = 0;
+    pub fn new(hours: i64, minutes: i64) -> Self {
+        let minutes = (((hours * MINUTES_PER_HOUR + minutes) % MINUTES_PER_DAY) + MINUTES_PER_DAY)
+            % MINUTES_PER_DAY;
 
-        res_minutes = minutes % MINUTES_PER_HOUR;
-        if res_minutes < 0 {
-            res_minutes += MINUTES_PER_HOUR;
-            // res_minutes < 0，说明还需要向res_hours再借1位
-            res_hours -= 1;
-        }
-
-        res_hours = (res_hours + minutes / MINUTES_PER_HOUR + hours) % HOURS_PER_DAY;
-        if res_hours < 0 {
-            res_hours += HOURS_PER_DAY;
-        }
-
-        Clock {
-            hours: res_hours,
-            minutes: res_minutes,
-        }
+        Clock { minutes }
     }
 
-    pub fn add_minutes(&self, minutes: i32) -> Self {
-        let total_minutes = self.hours * MINUTES_PER_HOUR + self.minutes + minutes;
-
-        Clock::new(0, total_minutes)
+    pub fn add_minutes(&self, minutes: i64) -> Self {
+        Clock::new(0, self.minutes + minutes)
     }
 }
 
 impl Display for Clock {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:02}:{:02}", self.hours, self.minutes)
+        write!(
+            f,
+            "{:02}:{:02}",
+            self.minutes / MINUTES_PER_HOUR,
+            self.minutes % MINUTES_PER_HOUR
+        )
     }
 }
