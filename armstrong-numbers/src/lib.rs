7633bad3e9cab4e2351805fc0061a201a9cea9a2
mike <lixiaofeng1115@outlook.com>
2023-06-19 22:35:29 +0800

    13: armstrong numbers, iter once

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,18 +1,17 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    let rems = convert_num_to_digits(num);
-    let len = rems.len() as u32;
+    let digits_count = (num as f64).log10() as u32 + 1;
 
-    rems.iter().map(|&x| x.pow(len)).sum::<u64>() == num as u64
+    convert_num_to_digits_and_calc(num, digits_count) == num as u64
 }
 
-fn convert_num_to_digits(mut num: u32) -> Vec<u64> {
-    let mut rems = vec![];
+fn convert_num_to_digits_and_calc(mut num: u32, digits_count: u32) -> u64 {
+    let mut sum = 0_u64;
 
     while num > 0 {
         let rem = (num % 10) as u64;
-        rems.push(rem);
+        sum += rem.pow(digits_count);
         num /= 10;
     }
 
-    rems
+    sum
 }
