bbd0f37d7de83a421c2e4f3b351887569f7e37dd
mike <lixiaofeng1115@outlook.com>
2023-06-21 17:54:22 +0800

    15: difference of squares

---
+++ difference-of-squares/src/lib.rs
@@ -0,0 +1,12 @@
+pub fn square_of_sum(n: u32) -> u32 {
+    let sum = (1..=n).sum::<u32>();
+    sum * sum
+}
+
+pub fn sum_of_squares(n: u32) -> u32 {
+    (1..=n).map(|n| n * n).sum::<u32>()
+}
+
+pub fn difference(n: u32) -> u32 {
+    square_of_sum(n) - sum_of_squares(n)
+}
