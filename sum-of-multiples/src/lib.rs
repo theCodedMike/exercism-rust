880a26ae723e990cb4e1fcd9b4234e9153e75bae
mike <lixiaofeng1115@outlook.com>
2023-06-28 23:02:07 +0800

    22: sum of multiples

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,9 +1,29 @@
+use std::collections::HashSet;
+
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
     if factors.is_empty() {
         return 0;
     }
 
-    (1..limit)
-        .filter(|&x| factors.iter().filter(|&&f| f != 0).any(|&f| x % f == 0))
-        .sum()
+    let mut res = HashSet::new();
+    for i in 1..u32::MAX {
+        let mut all_greater_than_limit = true;
+
+        for &f in factors {
+            if f == 0 {
+                continue;
+            }
+            let product = i * f;
+            if product < limit {
+                all_greater_than_limit = false;
+                res.insert(product);
+            }
+        }
+
+        if all_greater_than_limit {
+            break;
+        }
+    }
+
+    res.iter().sum()
 }
