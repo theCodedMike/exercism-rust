9c5f8a3f5a3add4f2d166b12ee38be4c697d23c9
mike <lixiaofeng1115@outlook.com>
2023-07-15 20:31:16 +0800

    41: pangram, optimize

--- pangram/src/lib.rs
+++ pangram/src/lib.rs
@@ -1,21 +1,12 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
-    let mut counter = ('a'..='z').map(|c| (c, 0)).collect::<HashMap<char, i32>>();
-
-    for c in sentence.chars() {
-        match c {
-            'a'..='z' | 'A'..='Z' => {
-                let mut c_clone = c;
-                if c_clone.is_ascii_uppercase() {
-                    c_clone = c_clone.to_ascii_lowercase();
-                }
-                counter.entry(c_clone).and_modify(|v| *v += 1).or_insert(0);
-            }
-            _ => {}
-        }
-    }
-
-    counter.values().all(|v| *v >= 1)
+    sentence
+        .to_ascii_lowercase()
+        .chars()
+        .filter(|c| c.is_ascii_alphabetic())
+        .collect::<HashSet<_>>()
+        .len()
+        == 26
 }
