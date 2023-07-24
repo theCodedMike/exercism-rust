1145618ffb52331f1b25417da1f9be02c542a073
mike <lixiaofeng1115@outlook.com>
2023-07-25 04:50:37 +0800

    49: say, recursive version

--- say/src/lib.rs
+++ say/src/lib.rs
@@ -1,97 +1,62 @@
-use std::collections::HashMap;
-
-pub fn encode(mut n: u64) -> String {
-    if n == 0 {
-        return String::from("zero");
-    }
-
-    let scale_map: HashMap<i32, &str> = HashMap::from([
-        (1, " thousand"),
-        (2, " million"),
-        (3, " billion"),
-        (4, " trillion"),
-        (5, " quadrillion"),
-        (6, " quintillion"),
-    ]);
-
-    let special_num_map: HashMap<u64, &str> = HashMap::from([
-        (1, "one"),
-        (2, "two"),
-        (3, "three"),
-        (4, "four"),
-        (5, "five"),
-        (6, "six"),
-        (7, "seven"),
-        (8, "eight"),
-        (9, "nine"),
-        (10, "ten"),
-        (11, "eleven"),
-        (12, "twelve"),
-        (13, "thirteen"),
-        (14, "fourteen"),
-        (15, "fifteen"),
-        (16, "sixteen"),
-        (17, "seventeen"),
-        (18, "eighteen"),
-        (19, "nineteen"),
-        (20, "twenty"),
-        (30, "thirty"),
-        (40, "forty"),
-        (50, "fifty"),
-        (60, "sixty"),
-        (70, "seventy"),
-        (80, "eighty"),
-        (90, "ninety"),
-        (100, " hundred"),
-    ]);
-
-    let mut res = "".to_string();
-
-    let mut div_count = 0;
-    while n > 0 {
-        let rem = n % 1000;
-        if rem > 0 {
-            let sub_word = process_rem(rem, &special_num_map);
-            res.insert_str(0, &sub_word);
+const ONES: [&str; 20] = [
+    "zero",
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+    "ten",
+    "eleven",
+    "twelve",
+    "thirteen",
+    "fourteen",
+    "fifteen",
+    "sixteen",
+    "seventeen",
+    "eighteen",
+    "nineteen",
+];
+const TENS: [&str; 10] = [
+    "zero", "ten", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety",
+];
+const ORDERS: [&str; 7] = [
+    "zero",
+    "thousand",
+    "million",
+    "billion",
+    "trillion",
+    "quadrillion",
+    "quintillion", // enough for u64::MAX
+];
+pub fn encode(num: u64) -> String {
+    match num {
+        0..=19 => ONES[num as usize].to_string(),
+        20..=99 => {
+            let upper = (num / 10) as usize;
+            match num % 10 {
+                0 => TENS[upper].to_string(),
+                lower => format!("{}-{}", TENS[upper], encode(lower)),
+            }
         }
-        n /= 1000;
-        div_count += 1;
-        if n > 0 && rem > 0 {
-            res.insert(0, ' ');
-        }
-        if n > 0 && n % 1000 != 0 {
-            res.insert_str(0, scale_map[&div_count]);
+        100..=999 => format_num(num, 100, "hundred"),
+        _ => {
+            let (div, order) = std::iter::successors(Some(1u64), |v| v.checked_mul(1000))
+                .zip(ORDERS.iter())
+                .find(|&(e, _)| e > num / 1000)
+                .unwrap();
+            format_num(num, div, order)
         }
     }
-
-    res
 }
-
-fn process_rem(mut rem: u64, special_num_map: &HashMap<u64, &str>) -> String {
-    let mut sub_word = "".to_string();
-    let mut rem_ge_100 = false;
-    if rem >= 100 {
-        rem_ge_100 = true;
-        sub_word.push_str(special_num_map[&(rem / 100)]);
-        sub_word.push_str(special_num_map[&100]);
-        rem %= 100;
-    }
-    if rem > 0 {
-        if rem_ge_100 {
-            sub_word.push(' ');
-        }
-        match special_num_map.get(&rem) {
-            None => {
-                let most = rem / 10 * 10; // 23 -> 20
-                sub_word.push_str(special_num_map[&most]);
-                sub_word.push('-');
-                sub_word.push_str(special_num_map[&(rem - most)]); // 23 - 20 == 3
-            }
-            Some(&val) => {
-                sub_word.push_str(val);
-            }
+fn format_num(num: u64, div: u64, order: &str) -> String {
+    match (num / div, num % div) {
+        (upper, 0) => format!("{} {}", encode(upper), order),
+        (upper, lower) => {
+            format!("{} {} {}", encode(upper), order, encode(lower))
         }
     }
-
-    sub_word
 }
