183fe7ef52a7a6440341aa5ed2a99179a50ee6b8
mike <lixiaofeng1115@outlook.com>
2023-08-02 06:32:09 +0800

    57: variable length quantity, optimize

--- variable-length-quantity/src/lib.rs
+++ variable-length-quantity/src/lib.rs
@@ -4,137 +4,39 @@ pub enum Error {
     Overflow,
 }
 
-const U32_MAX: u64 = u32::MAX as u64;
-
 /// Convert a list of numbers to a stream of bytes encoded with variable length encoding.
 pub fn to_bytes(values: &[u32]) -> Vec<u8> {
-    values
-        .into_iter()
-        .flat_map(|v| process_single(*v))
-        .collect()
-}
-
-fn process_single(mut ori: u32) -> Vec<u8> {
-    if ori == 0 {
-        return vec![0];
-    }
-    // to binary
-    let mut len = ori.ilog2() as usize + 1;
-    len = 7 * if len % 7 == 0 { len / 7 } else { (len / 7) + 1 };
-    let mut bits = vec![0; len];
-    for i in (0..len).rev() {
-        bits[i] = (ori % 2) as u8;
-        ori /= 2;
-        if ori == 0 {
-            break;
+    let mut res = Vec::new();
+    values.iter().rev().for_each(|&num| {
+        res.push(num as u8 & 0x7F);
+        let mut n = num >> 7;
+        while n != 0 {
+            res.push((n as u8 & 0x7F) | 0x80);
+            n >>= 7;
         }
-    }
-
-    // group by 7 bits
-    let last_idx = len / 7 - 1;
-    bits = bits
-        .chunks(7)
-        .enumerate()
-        .flat_map(|(idx, v)| {
-            let mut vec = v.to_vec();
-            if idx == last_idx {
-                vec.insert(0, 0);
-            } else {
-                vec.insert(0, 1);
-            }
-            vec
-        })
-        .collect::<Vec<_>>();
-
-    // to dec
-    let res = bits
-        .chunks(8)
-        .map(|v| {
-            let mut dec = 0;
-
-            for (idx, val) in v.iter().enumerate() {
-                if *val == 1 {
-                    dec += 2_u8.pow((7 - idx) as u32);
-                }
-            }
-
-            dec
-        })
-        .collect::<Vec<_>>();
-
+    });
+    res.reverse();
     res
 }
 
 /// Given a stream of bytes, extract all numbers which are encoded in there.
 pub fn from_bytes(bytes: &[u8]) -> Result<Vec<u32>, Error> {
-    // to binary
-    let mut bits = bytes
-        .iter()
-        .map(|v| {
-            let mut val = *v;
-            let mut vec = vec![0; 8];
-            for i in (0..8).rev() {
-                vec[i] = val % 2;
-                val /= 2;
-                if val == 0 {
-                    break;
-                }
-            }
-            vec
-        })
-        .collect::<Vec<_>>();
-
-    // group
-    let groups = bits
-        .split_inclusive_mut(|v| v[0] == 0)
-        .map(|v| {
-            let mut group = vec![];
-
-            for i in v {
-                group.append(i);
-            }
-
-            group
-        })
-        .collect::<Vec<_>>();
-
-    let mut res = vec![];
-    for group in groups {
-        let sub_res = handle_single(group)?;
-        res.push(sub_res);
-    }
-
-    Ok(res)
-}
-
-fn handle_single(bits: Vec<u8>) -> Result<u32, Error> {
-    // validate
-    let bits_len = bits.len();
-    if bits[bits_len - 8] == 1 {
-        return Err(Error::IncompleteNumber);
-    }
-
-    // remove 1 or 0
-    let real_bits = bits
-        .chunks(8)
-        .flat_map(|v| {
-            let mut vec = v.to_vec();
-            vec.remove(0);
-            vec
-        })
-        .collect::<Vec<_>>();
-
-    let mut dec = 0_u64;
-    let len = real_bits.len() - 1;
-    for (idx, val) in real_bits.iter().enumerate() {
-        if *val == 1 {
-            let pow = 2_u64.pow((len - idx) as u32);
-            if dec + pow > U32_MAX {
-                return Err(Error::Overflow);
-            }
-            dec += pow;
+    let mut res = Vec::new();
+    let mut iter = bytes.iter().peekable();
+    let mut number = 0u32;
+    while let Some(byte) = iter.next() {
+        number = if number.leading_zeros() >= 7 {
+            (number << 7) | (byte & 0x7F) as u32
+        } else {
+            return Err(Error::Overflow);
+        };
+        if byte & 0x80 == 0 {
+            res.push(number);
+            number = 0;
+            continue;
+        } else if iter.peek().is_none() {
+            return Err(Error::IncompleteNumber);
         }
     }
-
-    Ok(dec as u32)
+    Ok(res)
 }
