86167516fde281040bcbc75ea1983d0d941812ef
mike <lixiaofeng1115@outlook.com>
2023-08-05 22:16:13 +0800

    61: ocr numbers, optimize

--- ocr-numbers/src/lib.rs
+++ ocr-numbers/src/lib.rs
@@ -3,6 +3,9 @@
 
 use std::ops::Index;
 
+const ROW_COUNT: usize = 4;
+const COLUMN_COUNT: usize = 3;
+
 #[derive(Debug, PartialEq, Eq)]
 pub enum Error {
     InvalidRowCount(usize),
@@ -12,65 +15,68 @@ pub enum Error {
 pub fn convert(input: &str) -> Result<String, Error> {
     let lines = input.split('\n').collect::<Vec<_>>();
     let rows = lines.len();
-    if rows % 4 != 0 {
+    if rows % ROW_COUNT != 0 {
         return Err(Error::InvalidRowCount(rows));
     }
     let cols = lines[0].len();
-    if cols % 3 != 0 {
+    if cols % COLUMN_COUNT != 0 {
         return Err(Error::InvalidColumnCount(cols));
     }
 
     let mut res = "".to_string();
-    lines.chunks(4).enumerate().for_each(|(idx, _4_lines)| {
-        if idx > 0 {
-            res.push(',');
-        }
-        let mut col_idx = 0;
-        while col_idx != cols {
-            let range = col_idx..col_idx + 3;
-            let digit = process_single_digit(
-                _4_lines[0].index(range.clone()),
-                _4_lines[1].index(range.clone()),
-                _4_lines[2].index(range),
-            );
-            res.push(digit);
-            col_idx += 3;
-        }
-    });
+    lines
+        .chunks(ROW_COUNT)
+        .enumerate()
+        .for_each(|(idx, _4_lines)| {
+            if idx > 0 {
+                res.push(',');
+            }
+            let mut col_idx = 0;
+            while col_idx != cols {
+                let range = col_idx..col_idx + COLUMN_COUNT;
+                let digit = process_single_digit(
+                    _4_lines[0].index(range.clone()),
+                    _4_lines[1].index(range.clone()),
+                    _4_lines[2].index(range),
+                );
+                res.push(digit);
+                col_idx += COLUMN_COUNT;
+            }
+        });
 
     Ok(res)
 }
 
 fn process_single_digit(line_1: &str, line_2: &str, line_3: &str) -> char {
-    if line_1 == " _ " && line_2 == "| |" && line_3 == "|_|" {
-        return '0';
+    match line_1 {
+        "   " => match line_2 {
+            "  |" => '1',
+            "|_|" => '4',
+            _ => '?',
+        },
+        " _ " => match line_2 {
+            "  |" => '7',
+            "| |" => match line_3 {
+                "|_|" => '0',
+                _ => '?',
+            },
+            " _|" => match line_3 {
+                "|_ " => '2',
+                " _|" => '3',
+                _ => '?',
+            },
+            "|_ " => match line_3 {
+                "|_|" => '6',
+                " _|" => '5',
+                _ => '?',
+            },
+            "|_|" => match line_3 {
+                "|_|" => '8',
+                " _|" => '9',
+                _ => '?',
+            },
+            _ => '?',
+        },
+        _ => '?',
     }
-    if line_1 == "   " && line_2 == "  |" && line_3 == "  |" {
-        return '1';
-    }
-    if line_1 == " _ " && line_2 == " _|" && line_3 == "|_ " {
-        return '2';
-    }
-    if line_1 == " _ " && line_2 == " _|" && line_3 == " _|" {
-        return '3';
-    }
-    if line_1 == "   " && line_2 == "|_|" && line_3 == "  |" {
-        return '4';
-    }
-    if line_1 == " _ " && line_2 == "|_ " && line_3 == " _|" {
-        return '5';
-    }
-    if line_1 == " _ " && line_2 == "|_ " && line_3 == "|_|" {
-        return '6';
-    }
-    if line_1 == " _ " && line_2 == "  |" && line_3 == "  |" {
-        return '7';
-    }
-    if line_1 == " _ " && line_2 == "|_|" && line_3 == "|_|" {
-        return '8';
-    }
-    if line_1 == " _ " && line_2 == "|_|" && line_3 == " _|" {
-        return '9';
-    }
-    return '?';
 }
