291664b063e4915ae934dd39cdf86e6a44af411d
mike <lixiaofeng1115@outlook.com>
2023-08-10 21:27:09 +0800

    66: rectangles, todo!

---
+++ rectangles/src/lib.rs
@@ -0,0 +1,4 @@
+pub fn count(lines: &[&str]) -> u32 {
+    // todo!
+    unimplemented!("\nDetermine the count of rectangles in the ASCII diagram represented by the following lines:\n{lines:#?}\n.");
+}
