ad3174aa1616e4162ac2fe404f26ba594f1c8da1
mike <lixiaofeng1115@outlook.com>
2023-08-28 05:06:22 +0800

    80: roman numerals, optimize

--- roman-numerals/src/lib.rs
+++ roman-numerals/src/lib.rs
@@ -1,51 +1,37 @@
-use std::collections::HashMap;
 use std::fmt::{Display, Formatter, Result};
-use std::sync::OnceLock;
 
-static INIT: OnceLock<HashMap<usize, &str>> = OnceLock::new();
+const NUMERALS: [(usize, [&'static str; 10]); 4] = [
+    (
+        1000,
+        ["", "M", "MM", "MMM", "--", "-", "--", "---", "----", "--"],
+    ),
+    (
+        100,
+        ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
+    ),
+    (
+        10,
+        ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
+    ),
+    (
+        1,
+        ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+    ),
+];
 
 pub struct Roman(usize);
 
 impl Display for Roman {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
-        let roman_map = INIT.get_or_init(|| {
-            HashMap::from([
-                (1, "I"),
-                (5, "V"),
-                (10, "X"),
-                (50, "L"),
-                (100, "C"),
-                (500, "D"),
-                (1000, "M"),
-            ])
-        });
-
-        let mut res = "".to_string();
-        let mut val = self.0;
-        let mut base = 1;
-        while val != 0 {
-            let rem = val % 10;
-            if rem != 0 {
-                let curr_val = rem * base;
-                let sub_res = if roman_map.contains_key(&curr_val) {
-                    roman_map[&curr_val].to_string()
-                } else {
-                    match rem {
-                        2..=3 => roman_map[&base].repeat(rem),
-                        4 => roman_map[&base].to_string() + roman_map[&(5 * base)],
-                        6..=8 => {
-                            roman_map[&(5 * base)].to_string() + &roman_map[&base].repeat(rem - 5)
-                        }
-                        9 => roman_map[&base].to_string() + roman_map[&(base * 10)],
-                        _ => panic!("unsupported rem: {}", rem),
-                    }
-                };
-                res.insert_str(0, &sub_res);
-            }
-
-            base *= 10;
-            val /= 10;
-        }
+        let val = self.0;
+        let res = if val > 3999 {
+            "".to_string()
+        } else {
+            NUMERALS
+                .iter()
+                .map(|&(base, nums)| nums[(val / base) % 10])
+                .collect()
+        };
 
         write!(f, "{}", res)
     }
