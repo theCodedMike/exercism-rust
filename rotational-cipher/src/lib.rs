8570512ccf564f2538edad01ea95e5687ab7b116
mike <lixiaofeng1115@outlook.com>
2023-08-28 23:33:00 +0800

    81: rotational cipher, optimize

--- rotational-cipher/src/lib.rs
+++ rotational-cipher/src/lib.rs
@@ -1,46 +1,19 @@
-use std::collections::HashMap;
-
-#[derive(Debug, PartialEq, Eq)]
-enum RotateDirection {
-    Left,
-    None,
-    Right,
+pub fn rotate(input: &str, key: i8) -> String {
+    input
+        .chars()
+        .map(|c| match c {
+            'a'..='z' => rot(c as u8, b'a', key),
+            'A'..='Z' => rot(c as u8, b'A', key),
+            _ => c,
+        })
+        .collect()
 }
 
-pub fn rotate(input: &str, key: i8) -> String {
-    let (direction, dist) = match key % 26 {
-        lt_0 if lt_0 < 0 => (RotateDirection::Right, lt_0 + 26),
-        gt_0 if gt_0 > 0 => (RotateDirection::Left, gt_0),
-        _ => (RotateDirection::None, 0),
-    };
-    if direction == RotateDirection::None {
-        input.to_string()
+fn rot(c: u8, lower_bound: u8, key: i8) -> char {
+    let new = (c - lower_bound) as i8 + key;
+    if new < 0 {
+        ((new + 26) as u8 + lower_bound) as char
     } else {
-        let mut a_to_z = ('a'..='z').collect::<Vec<_>>();
-        a_to_z.rotate_left(dist as usize);
-        let map = ('a'..='z')
-            .zip(a_to_z.into_iter())
-            .collect::<HashMap<char, char>>();
-
-        input
-            .chars()
-            .map(|c| {
-                if c.is_ascii_alphabetic() {
-                    let (lower, is_upper) = if c.is_ascii_uppercase() {
-                        (c.to_ascii_lowercase(), true)
-                    } else {
-                        (c, false)
-                    };
-
-                    let mut res = map[&lower];
-                    if is_upper {
-                        res = res.to_ascii_uppercase();
-                    }
-                    res
-                } else {
-                    c
-                }
-            })
-            .collect()
+        (new as u8 % 26 + lower_bound) as char
     }
 }
