ce1a66d0352c15466f3119af3e4aea893d8cba53
mike <lixiaofeng1115@outlook.com>
2023-09-02 01:43:25 +0800

    85: pythagorean triplet, optimize

--- pythagorean-triplet/src/lib.rs
+++ pythagorean-triplet/src/lib.rs
@@ -2,16 +2,15 @@ use std::collections::HashSet;
 
 pub fn find(sum: u32) -> HashSet<[u32; 3]> {
     let mut set = HashSet::new();
-    let half = sum / 2;
 
-    for i in 1..=half {
-        for j in i..=half {
-            let k = sum - i - j;
-            if k < j {
-                continue;
-            }
-            if i * i + j * j == k * k {
-                set.insert([i, j, k]);
+    for a in 1..=sum / 3 {
+        let sum_b_c = sum - a;
+        let denominator = 2 * sum_b_c;
+        let numerator = sum_b_c * sum_b_c - a * a;
+        if numerator % denominator == 0 {
+            let b = numerator / denominator;
+            if a < b {
+                set.insert([a, b, sum - a - b]);
             }
         }
     }
