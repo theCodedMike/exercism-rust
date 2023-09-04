1fe0029762cda6559735b6459094a782ce4b1f18
mike <lixiaofeng1115@outlook.com>
2023-09-04 14:21:11 +0800

    87: book store, todo!

---
+++ book-store/src/lib.rs
@@ -0,0 +1,6 @@
+///
+/// Find the lowest price of the bookbasket with books {books:?}
+///
+pub fn lowest_price(books: &[u32]) -> u32 {
+    unimplemented!("Find the lowest price of the bookbasket with books {books:?}")
+}
